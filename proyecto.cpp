c5c47d8e6eeb054cfdd4493921252e66a338be04
MiguelPenaCedeno <miguelangelpenacedeno@gmail.com>
2026-03-17 18:33:57 -0500

    primer cambio

---
+++ proyecto.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+struct equipo {
+    int codigo;
+    char nombre[30];
+    char laboratorio[40];
+    int tipo;
+    bool estado_operativo;
+    int costo_estimado;
+    int semestre_minimo;
+    char descripcion_tecnica[100];
+};
+
+struct usuario {
+
+};
+
+struct 
+
+int main () {
+
+
+    do {
+        switch(menu) {
+
+            case 1:
+
+            break;
+
+            case 2:
+
+            break;
+
+            case 3
+
+            break;
+
+        }
+
+    } while (menu != 0);
+}
