a7d337151bd764d3ac477c37998234da7d39d510
MiguelPenaCedeno <miguelangelpenacedeno@gmail.com>
2026-03-17 19:03:09 -0500

    tercer push

--- proyecto.cpp
+++ proyecto.cpp
@@ -30,29 +30,71 @@ struct sesion {
     bool penalizacion;
 }
 
-struct 
+void cargarEquipos();
+void cargarUsuarios();
+void consultarEstadoOperativo();
+void programarSesionUso();
+void registrarCierreSesion();
+void generarInformeUso();
+void rankingUsuarios();
+
 
 int main () {
 
-    int* ptr_equipo = &codigo_equipo;
-    int* ptr_institucional = &codigo_institucional;
+
+    cout << "Bienvenido al menu"; 
+    cout << "Oprima '1' para cargar equipos desde un archivo" << endl;
+    cout << "Oprima '2' para cargar usuarios desde un archivo" << endl;
+    cout << "Oprima '3' para consultar el estado operativo de un laboratorio" << endl;
+    cout << "Oprima '4' para programar una sesion de uso" << endl; 
+    cout << "Oprima '5' para registrar cierre de sesion y observaciones" << endl;
+    cout << "Oprima '6' para generar un informe de uso intensivo de equipos" << endl;
+    cout << "Oprima '7' para generar un ranking de usuarios criticos" << endl;
+    cout << "Oprima '8' para salir" << endl;
+    cout << "Ingrese su elección: ";
+    cin >> menu;
+
 
     do {
         switch(menu) {
 
             case 1:
-
+            cargarEquipos();
             break;
 
             case 2:
-
+            cargarUsuarios();
             break;
 
             case 3:
-
+            consultarEstadoOperativo();
             break;
 
+            case 4:
+            programarSesionUso();
+            break;
+
+            case 5:
+            registrarCierreSesion();
+            break;
+
+            case 6:
+            generarInformeUso();
+            break;
+
+            case 7:
+            generarRankingUsuarios();
+            break
+
+            case 8:
+            rankingUsuarios();
+            break;
+
+            default:
+            cout << "Valor ingresado no valido, intente nuevamente: ";
+            cin >> menu;
         }
 
     } while (menu != 0);
+    return 0;
 }
